c7cc6617381e1605646c2f3e7ca3cd05cc45716d
asz07 <asz07@mail.aub.edu>
2022-02-05 18:41:51 +0200

    displayed environment in the title

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -6,6 +6,7 @@ const Home = (props) => {
   const navigate = useNavigate();
   return (
     <div align='center' id='home'>
+      <h1>{process.env.NODE_ENV}</h1>
       <h1>Welcome home!</h1>
       <Button onClick={() => { navigate('/employee/add') }}>Add Employees</Button>
       <Button onClick={() => { navigate('/employee/all') }}>View Employees</Button>
