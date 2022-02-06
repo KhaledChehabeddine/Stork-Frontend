e08007df57f93e0bcfb0682188a2c166fb0316f8
asz07 <asz07@mail.aub.edu>
2022-02-07 01:41:19 +0200

    added loading while loading applicants and employees view page

--- src/Components/Cards/ApplicantCardWrapper.js
+++ src/Components/Cards/ApplicantCardWrapper.js
@@ -1,20 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import getApiClient from "../../api_client/getApiClient";
 import ApplicantCard from "./ApplicantCard";
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'page-loaded':
+      return { pageLoaded: true };
+    default:
+      return { ...state };
+  }
+}
+
 const ApplicantCardWrapper = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    pageLoaded: false
+  });
   const [applicants, setApplicants] = useState([]);
   const getAllApplicants = useEffect(() => {
     getApiClient().getAllApplicants()
       .then(response => {
         setApplicants(response.data);
+        dispatch({ type: 'page-loaded' });
       }).catch(error => {console.log(error)});
   }, []);
   return (
-    <div className='card_wrapper'>
-      {applicants.map(applicant => <ApplicantCard key={applicant.id} applicant={applicant} />)}
-    </div>
+    <>
+      {state.pageLoaded === true
+        ? <div style={{ display: 'flex', flexDirection: 'column'}}>
+            <h1 align='center' style={{ padding: '2rem' }}>Applicants</h1>
+            <div className='card_wrapper'>
+              {applicants.map(applicant => <ApplicantCard key={applicant.id} applicant={applicant} />)}
+            </div>
+          </div>
+        : <h1 align='center'>Loading...</h1>}
+    </>
+
   )
 }
+
+
 
 export default ApplicantCardWrapper;
