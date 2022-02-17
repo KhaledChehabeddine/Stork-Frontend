fa901c3b058b4fd8aeef0e352b97897da0714872
Lynne Ismail <lynneismael@hotmail.com>
2022-02-17 15:27:16 +0200

    styling forms

--- src/Components/Forms/EmployeeForm.js
+++ src/Components/Forms/EmployeeForm.js
@@ -25,36 +25,38 @@ const EmployeeForm = (props) => {
     employeeAdded: false
   });
   const [id, setId] = useState('');
-  const [name, setName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [jobTitle, setJobTitle] = useState('');
   const [phone, setPhone] = useState('');
   
   const addEmployeeHandler = useCallback(() => {
     dispatch({ type: 'adding-employee' });
-    getApiClient().addEmployee('/add', id, name, email, jobTitle, phone, 'https://bootdey.com/img/Content/avatar/avatar3.png')
+    getApiClient().addEmployee('/add', id, firstName, lastName, email, jobTitle, phone, 'https://bootdey.com/img/Content/avatar/avatar3.png')
       .then(response => {
         dispatch({ type: 'employee-added' });
         navigate(`/employee/all`);
       }).catch(error => {
         console.log(error);
       })
-  }, [id, name, email, jobTitle, phone, navigate]);
+  }, [id, firstName, lastName, email, jobTitle, phone, navigate]);
 
   return (
     <>
       <NavBar/>
       {state.addingEmployee ? <Spinner />
         :
-          <div align='center'>
+          <div className = "employee-form" align='center'>
             <Form onSubmit={event => { event.preventDefault(); }}>
               <h1 style={{ padding: '1rem' }}>Employee Adder</h1>
-              <Input onChange={event => { setId(event.target.value); }} placeholder='Id' />
-              <Input onChange={event => { setName(event.target.value); }} placeholder='Name' />
-              <Input onChange={event => { setEmail(event.target.value); }} placeholder='Email' />
-              <Input onChange={event => { setJobTitle(event.target.value); }} placeholder='Job Title' />
-              <Input onChange={event => { setPhone(event.target.value); }} placeholder='Phone' />
-              <Button onClick={addEmployeeHandler}>Add Employee</Button>
+              <Input className="employee-form-ID" onChange={event => { setId(event.target.value); }} placeholder='ID' />
+              <Input className="employee-form-firstName" onChange={event => { setFirstName(event.target.value); }} placeholder='First Name' />
+              <Input className="employee-form-lastName" onChange={event => { setLastName(event.target.value); }} placeholder='Last Name' />
+              <Input className="employee-form-email" onChange={event => { setEmail(event.target.value); }} placeholder='Email' />
+              <Input className="employee-form-jobTitle" onChange={event => { setJobTitle(event.target.value); }} placeholder='Job Title' />
+              <Input className="employee-form-phone" onChange={event => { setPhone(event.target.value); }} placeholder='Phone Number' />
+              <Button className="employee-form-saveChanges" onClick={addEmployeeHandler}>Add Employee</Button>
             </Form>
           </div>}
     </>
