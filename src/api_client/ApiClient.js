d0f1f1ee300abd5ce7e4593407b235ed5c8f1a7a
Khaled Chehabeddine <khaled.ch2002@gmail.com>
2022-04-15 18:32:47 +0300

    Adding job position to Candidate Form, changed countries to be a dictionary of countries, updated addCandidate in apiClient to support new parameters

--- src/api_client/ApiClient.js
+++ src/api_client/ApiClient.js
@@ -43,14 +43,16 @@ class ApiClient extends ApiClientBase {
     return this.Get('/candidate/find', params);
   }
 
-  addCandidate(firstName, lastName, country, sex, email, phone) {
+  addCandidate(firstName, lastName, country, countryPhone, gender, email, phone, jobPosition) {
     const data = {};
     data.firstName = firstName;
     data.lastName = lastName;
     data.country = country;
-    data.sex = sex;
+    data.countryPhone = countryPhone;
+    data.gender = gender;
     data.email = email;
     data.phone = phone;
+    data.jobPosition = jobPosition;
     data.date = getCurrentDate();
     return this.Post('/candidate/add', data);
   }
